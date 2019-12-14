9096da1f38d8fc52dee02b5fb22381f77c14c89c
Chethan Yadav <cyadav@sonicwall.com>
2019-12-14 06:51:08 +0530

    My Go codes

---
+++ src/test.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var k bool
+	k = primePredictor(31)
+	fmt.Printf("the given number is prime %t", k)
+	var str1 = "Helloworld"
+	var str2 = "ByeWorld"
+	fmt.Printf("The value when compared", string.compare(str1, str2))
+}
+func primePredictor(num int) bool {
+	if num == 0 {
+		return false
+	}
+	if num == 1 {
+		return true
+	}
+	count := 0
+	for j := 2; j <= num/2; j++ {
+		if num%j == 0 {
+			count++
+		}
+	}
+	if count == 0 {
+		return true
+	}
+	return false
+}
