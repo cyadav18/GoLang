7308eaed8bd172d16b4bdbe14defb388bb210212
Chethan <chethanyadav5@gmail.com>
2019-12-20 05:59:05 +0530

    modifing go code

--- src/test.go
+++ src/test.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type Hotdog int
+type hotdog int
 
-var x Hotdog
+var x hotdog
 var y int
+var number = []int{}
+var exp = []int{}
+
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
 
 func main() {
 	var k bool
@@ -17,8 +29,46 @@ func main() {
 	fmt.Printf("The value of x is %d\n", x)
 	fmt.Printf("The type of y %T \n", y)
 	fmt.Printf("The value of y is %d\n", y)
+	a := []int{1, 2, 3, 4, 5, 6}
+	b := []int{1}
+	fmt.Println(a)
+	fmt.Println(b)
+	b = append(b, kb, mb/kb, gb/(mb))
+	a = append(a, b...)
+	fmt.Println(a)
+	primefactor(126)
+	fmt.Println(number, "\n", exp)
 }
 
+func primefactor(num int) {
+	if primePredictor(num) {
+		number = append(number, num)
+		exp = append(exp, 1)
+	} else {
+		for i := 2; num != 1; i++ {
+			if primePredictor(i) && num%i == 0 {
+				number = append(number, i)
+				exp = append(exp, getnumExpo(num, i))
+				fst := float64(number[len(number)-1])
+				snd := float64(exp[len(exp)-1])
+				num = num / int(math.Pow(fst, snd))
+			}
+		}
+	}
+}
+
+func getnumExpo(num1, num2 int) int {
+	count := 0
+	for num1 != 0 {
+		if num1%num2 == 0 {
+			count++
+			num1 = num1 / num2
+		} else {
+			return count
+		}
+	}
+	return -1
+}
 func primePredictor(num int) bool {
 	if num == 0 {
 		return false
